c587bca3b3ddf66bc2c5f91bc9e0743cf705bc57
lekayo <39109527+lekayo@users.noreply.github.com>
2019-02-12 18:09:37 +1100

    Update slow-timer.ps1

--- slow-timer.ps1
+++ slow-timer.ps1
@@ -1,5 +1,1 @@
 Get-ChildItem HKLM:/SYSTEM/CurrentControlSet/Services/MsLbfoProvider/Parameters/NdisAdapters | New-ItemProperty -Name LacpTimeout -PropertyType DWORD -Value 0
-
-
-
-restart-computer
