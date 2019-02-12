78414e6c9b228c610d74fe9f2f3f799d706ee4d4
lekayo <39109527+lekayo@users.noreply.github.com>
2019-02-12 18:08:11 +1100

    Add files via upload

---
+++ slow-timer.ps1
@@ -0,0 +1,5 @@
+Get-ChildItem HKLM:/SYSTEM/CurrentControlSet/Services/MsLbfoProvider/Parameters/NdisAdapters | New-ItemProperty -Name LacpTimeout -PropertyType DWORD -Value 0
+
+
+
+restart-computer
