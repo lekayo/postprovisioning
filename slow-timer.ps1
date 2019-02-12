7634e6314f1d3ceada99b3393a04d83b641b5b1d
lekayo <39109527+lekayo@users.noreply.github.com>
2019-02-12 16:22:57 +1100

    Delete slow-timer.ps1

--- slow-timer.ps1
+++
@@ -1,9 +0,0 @@
-powershell
-
-
-
-Get-ChildItem HKLM:/SYSTEM/CurrentControlSet/Services/MsLbfoProvider/Parameters/NdisAdapters | New-ItemProperty -Name LacpTimeout -PropertyType DWORD -Value 0
-
-
-
-restart-computer
